a41a108d2beab432e7530240f5893871919868f7
chaeriIM <tldzmvpzl@naver.com>
2023-06-29 15:13:13 +0900

    웹 기본 구성

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const API_KEY = process.env.REACT_APP_WEATHER_KEY;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}`
 
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Seoul</p>
+          </div>
+          <div className="temp">
+            <h1>23.7°</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>체감 온도 26.4°</p>
+          </div>
+          <div className="humidity">
+            <p>습도 91%</p>
+          </div>
+          <div className="wind">
+            <p>풍속 2.2m/s</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
